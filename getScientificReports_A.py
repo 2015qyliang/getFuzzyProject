541d7a44675ce0d8f1546bba59f9e1dd78775ab7
qyliang <qfsfdxlqy@163.com>
2018-11-03 19:28:17 +0800

    Update getScientificReports_A.py

--- getScientificReports_A.py
+++ getScientificReports_A.py
@@ -4,7 +4,7 @@
 # Description:
 # 1) download papers from "Scientific Reportsvolume"
 # 2) parsing pdf file and extract fuzzy projectNumber, related microbial community study
-# using deome: python get_FrontersMicrobiology.py 2018 1 1700
+# using deome: python getScientificReports_A.py 2016 18408 39929
 '''
 # version-A ---- This script
 # https://www.nature.com/articles/srep03569
